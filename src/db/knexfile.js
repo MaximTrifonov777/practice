71d5a0afaaa8adbe0d738996917941cf84877c4d
MaximTrifonov777 <legendariys11@gmail.com>
2026-06-09 21:29:45 +0300

    chore: initial project setup with dependencies and knex configuration

---
+++ src/db/knexfile.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.DB_PORT || 5432,
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'todo_tdd'
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+};
